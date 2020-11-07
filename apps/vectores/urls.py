4f47087797b33a0513d999eea1424dc571089148
Juan Pablo Perez <perezsantosjuanpablo@gmail.com>
2020-11-07 18:19:17 -0500

    Agregando ejemplos vectores

--- apps/vectores/urls.py
+++ apps/vectores/urls.py
@@ -5,5 +5,11 @@ app_name = 'vectores_app'
 
 
 urlpatterns = [
-    path('vectores',views.vectores.as_view(),name = 'vectores'),
+    path('vectores', views.vectores.as_view(), name='vectores'),
+    path('vectores/ejemplo-1', views.ejemplo1.as_view(), name='ejemplo1'),
+    path('vectores/ejemplo-2', views.ejemplo2.as_view(), name='ejemplo2'),
+    path('vectores/ejemplo-3', views.ejemplo3.as_view(), name='ejemplo3'),
+    path('vectores/ejemplo-4', views.ejemplo4.as_view(), name='ejemplo4'),
+    path('vectores/ejemplo-5', views.ejemplo5.as_view(), name='ejemplo5'),
+    path('vectores/ejemplo-6', views.ejemplo6.as_view(), name='ejemplo6'),
 ]
--- apps/vectores/views.py
+++ apps/vectores/views.py
@@ -8,3 +8,27 @@ from django.views.generic import(
 
 class vectores(TemplateView):
     template_name = "teoria/vectores/vectores.html"
+
+
+class ejemplo1(TemplateView):
+    template_name = "teoria/vectores/ejemplos/ejemplo1.html"
+
+
+class ejemplo2(TemplateView):
+    template_name = "teoria/vectores/ejemplos/ejemplo2.html"
+
+
+class ejemplo3(TemplateView):
+    template_name = "teoria/vectores/ejemplos/ejemplo3.html"
+
+
+class ejemplo4(TemplateView):
+    template_name = "teoria/vectores/ejemplos/ejemplo4.html"
+
+
+class ejemplo5(TemplateView):
+    template_name = "teoria/vectores/ejemplos/ejemplo5.html"
+
+
+class ejemplo6(TemplateView):
+    template_name = "teoria/vectores/ejemplos/ejemplo6.html"
