f44bde23aa3eccdd256a983cf84c82d05105bd11
Juan Pablo Perez <perezsantosjuanpablo@gmail.com>
2020-11-18 21:21:23 -0500

    Agregando cambios

--- apps/matrices/views.py
+++ apps/matrices/views.py
@@ -8,3 +8,27 @@ from django.views.generic import(
 
 class matrices(TemplateView):
     template_name = "teoria/matrices/matrices.html"
+
+
+class ejemplo1(TemplateView):
+    template_name = "teoria/matrices/ejemplos/ejemplo1.html"
+
+
+class ejemplo2(TemplateView):
+    template_name = "teoria/matrices/ejemplos/ejemplo2.html"
+
+
+class ejemplo3(TemplateView):
+    template_name = "teoria/matrices/ejemplos/ejemplo3.html"
+
+
+class ejemplo4(TemplateView):
+    template_name = "teoria/matrices/ejemplos/ejemplo4.html"
+
+
+class ejemplo5(TemplateView):
+    template_name = "teoria/matrices/ejemplos/ejemplo5.html"
+
+
+class ejemplo6(TemplateView):
+    template_name = "teoria/matrices/ejemplos/ejemplo6.html"
